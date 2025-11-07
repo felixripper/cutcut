e7f83e462a6342e8b5196fc1be8deeb69fd846ef
Felix Ripper <77970275+felixripper@users.noreply.github.com>
2025-11-07 10:17:12 +0000

    Update WalletConnect project ID

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const config = createConfig({
   connectors: [
     metaMask(),
     coinbaseWallet(),
-    walletConnect({ projectId: 'your_project_id' }),
+    walletConnect({ projectId: '25713046a3b7af4df595c1131b6819ed' }),
     injected(),
   ],
   transports: {
